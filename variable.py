7c56955f1d711e6ff68982cfb7c43de290e09d43
TULASI-1903 <ravuritulasi19@gmail.com>
2026-05-12 20:41:20 -0700

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,49 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+a=10
+print(a)
+10
+tulasi=9
+ravuri=2
+print(tulasi+ravuri)
+... 11
+... _=9
+... print(_)
+... 9
+...  =7
+...  
+... SyntaxError: unexpected indent
+... @=9
+... SyntaxError: invalid syntax
+... city="vijaya wada"
+... print(city)
+... vijaya wada
+... print("city")
+... city
+... D="hello"
+... print(D)
+... hello
+... a=7;b=4
+... print(a,b)
+... 7 4
+... #for multiple values
+... a,c,b=9,0,8
+... print(a,b,c)
+... 9 8 0
+... #key words not allowed
+... if=7
+... SyntaxError: invalid syntax
+... #numbers are  not allowed
+... 99=00
+... SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+... 99=oop
+... SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+... #un pack
+... a=(78)
+... print(a)
+... 78
+... #for space
+... a=(100+""+di)
+... Traceback (most recent call last):
+...   File "<pyshell#28>", line 1, in <module>
